24f99fa5df023d344d2e1ebd87fd7055264d5aeb
mark Sahlgreen <mark@nozzy.org>
2019-11-26 12:14:56 +0100

    encryption

--- src/App.vue
+++ src/App.vue
@@ -7,180 +7,202 @@
 <script>
 import { get, set } from 'idb-keyval'
 
-    export default {
-        data() {
-            return {
-                credentials: {},
-            }
-        },
-        created() {
-            this.getStoredData()
-        },
-        methods: {
-            // Gets chats, contacts and credentials from idb. If they don't exist, creates empty ones.
-            getStoredData() {
-                get('chats').then(val => {
-                    if (val === undefined) {
-                        console.log('stored chats not found...')
-                        set('chats', {}).then(() => {
-                            console.log('empty chats list created.')
-                        })
-                    }
-                    else this.$store.state.chats = val
-                })
-                get('contacts').then(val => {
-                    if (val === undefined) {
-                        console.log('stored contacts not found...')
-                        set('contacts', {}).then(() => {
-                            console.log('empty contacts list created.')
-                        })
-                    }
-                    else this.$store.state.contacts = val
-                })
-                get('credentials').then(val => {
-                    if (val === undefined) {
-                        console.log('stored credentials not found...')
-                        this.generateCredentials()
-                    }
-                    else {
-                        this.$store.state.credentials = val
-						this.refreshToken() // TODO temp
-                        // Gets messages every 10 seconds
-                        console.log('starting to fetch messages...')
-						this.$options.interval = setInterval(this.getMessages, 10000)
-                    }
-                })
-            },
-            // Generates credentials for the user upon the first time opening the app.
-            generateCredentials() {
-                let random = require("randomstring")
-                let forge = require('node-forge')
-                forge.prime.generateProbablePrime(1024, (err, num) => {
-                    this.credentials = {
-                        id: random.generate(150), // random string (150 chars)
-                        password: random.generate(30), // random string (30 chars)
-                        secret: num.toString(), // large prime for generating encryption keys (1024 bits)
-                        lastReceived: 0, // id of the last message received, starts at 0
-                    }
-                    console.log('new credentials generated.')
-                    console.log(this.credentials)
-
-                    // Registers a user with these credentials
-                    this.register()
-
-                })
-            },
-            register() {
-                let userCredentials = {
-                    username: this.credentials.id,
-                    password: this.credentials.password,
-                }
-                this.$http.post(this.$store.state.apiURL + 'auth/register/', userCredentials).then((response) => {
-                    // Check if user exists
-                    if (response.body.hasOwnProperty('success') && response.body.success == false) {
-                        console.log("user already exists, generating new credentials...")
-                        this.generateCredentials()
-                    }
-                    // Any other error, no tokens
-                    else if (!response.body.refresh || !response.body.access) {
-                        console.log("tokens not received: ")
-                        console.log(response)
-                    }
-                    else {
-                        this.credentials.refreshToken = response.body.refresh
-                        this.credentials.accessToken = response.body.access
-
-                        // Stores credentials in idb
-                        set('credentials', this.credentials).then(() => {
-                            console.log('new credentials stored.')
-                            this.$store.state.credentials = this.credentials
-
-                            // Calls getMessages() every 10 seconds
-                            console.log('starting to fetch messages...')
-                            this.$options.interval = setInterval(this.getMessages, 10000)
-                        })
-                    }
-                }, error => {
-                    console.log("error:")
-                    console.log(error)
-                })
-            },
-            // Gets messages from the api, gets called periodically
-            getMessages() {
-				let options = {
-					headers: {
-						'Authorization': 'Bearer ' + this.$store.state.credentials.accessToken
-					}
+export default {
+	data() {
+		return {
+			credentials: {},
+		}
+	},
+	created() {
+		this.getStoredData()
+	},
+	methods: {
+		// Gets chats, contacts and credentials from idb. If they don't exist, creates empty ones.
+		getStoredData() {
+			get('chats').then(val => {
+				if (val === undefined) {
+					console.log('stored chats not found...')
+					set('chats', {}).then(() => {
+						console.log('empty chats list created.')
+					})
+				} else this.$store.state.chats = val
+			})
+			get('contacts').then(val => {
+				if (val === undefined) {
+					console.log('stored contacts not found...')
+					set('contacts', {}).then(() => {
+						console.log('empty contacts list created.')
+					})
+				} else this.$store.state.contacts = val
+			})
+			get('credentials').then(val => {
+				if (val === undefined) {
+					console.log('stored credentials not found...')
+					this.generateCredentials()
+				} else {
+					this.$store.state.credentials = val
+					this.refreshToken() // TODO temp
+					// Gets messages every 10 seconds
+					console.log('starting to fetch messages...')
+					this.$options.interval = setInterval(this.getMessages, 10000)
 				}
-				// TODO receive messages array
-                this.$http.post(this.$store.state.apiURL + 'receive/', {'last_message_id': this.$store.state.credentials.lastReceived}, options).then(response => {
-                	// Gets array of messages, for each message - check whether to store it
-                    for (let message of response.body) {
+			})
+		},
+		// Generates credentials for the user upon the first time opening the app.
+		generateCredentials() {
+			let random = require('randomstring')
+			let forge = require('node-forge')
+			forge.prime.generateProbablePrime(1024, (err, num) => {
+				this.credentials = {
+					id: random.generate(150), // random string (150 chars)
+					password: random.generate(30), // random string (30 chars)
+					secret: num.toString(), // large prime for generating encryption keys (1024 bits)
+					lastReceived: 0, // id of the last message received, starts at 0
+				}
+				console.log('new credentials generated.')
+				console.log(this.credentials)
+
+				// Registers a user with these credentials
+				this.register()
+			})
+		},
+		register() {
+			let userCredentials = {
+				username: this.credentials.id,
+				password: this.credentials.password,
+			}
+			this.$http
+				.post(this.$store.state.apiURL + 'auth/register/', userCredentials)
+				.then(
+					response => {
+						// Check if user exists
+						if (
+							response.body.hasOwnProperty('success') &&
+							response.body.success == false
+						) {
+							console.log('user already exists, generating new credentials...')
+							this.generateCredentials()
+						}
+						// Any other error, no tokens
+						else if (!response.body.refresh || !response.body.access) {
+							console.log('tokens not received: ')
+							console.log(response)
+						} else {
+							this.credentials.refreshToken = response.body.refresh
+							this.credentials.accessToken = response.body.access
+
+							// Stores credentials in idb
+							set('credentials', this.credentials).then(() => {
+								console.log('new credentials stored.')
+								this.$store.state.credentials = this.credentials
+
+								// Calls getMessages() every 10 seconds
+								console.log('starting to fetch messages...')
+								this.$options.interval = setInterval(this.getMessages, 10000)
+							})
+						}
+					},
+					error => {
+						console.log('error:')
+						console.log(error)
+					}
+				)
+		},
+		// Gets messages from the api, gets called periodically
+		getMessages() {
+			let options = {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.credentials.accessToken,
+				},
+			}
+			// TODO receive messages array
+			this.$http
+				.post(
+					this.$store.state.apiURL + 'receive/',
+					{ last_message_id: this.$store.state.credentials.lastReceived },
+					options
+				)
+				.then(response => {
+					// Gets array of messages, for each message - check whether to store it
+					for (let message of response.body) {
+						// Check and see if the sender in the message is in our contacts
 						if (this.$store.state.chats.hasOwnProperty(message.sender)) {
 							// TODO decrypt message.text here using this.$store.state.contacts[message.sender].key
+
+							// After decryption, we push the message onto our store
 							this.$store.state.chats[message.sender].push({
 								received: true,
-								content: message.text
+								content: message.text,
 							})
+							// then we update the last received id
 							this.$store.state.credentials.lastReceived = message.id
 						}
-                    }
-                    // Save messages and lastReceived in idb
+					}
+					// Save messages and lastReceived in idb
 					set('chats', this.$store.state.chats).then(() => {
 						console.log('chats saved.')
 					})
 					set('credentials', this.$store.state.credentials).then(() => {
 						console.log('credentials saved.')
 					})
-                })
-            },
-			// Gets user info by providing a token
-            getInfo() {
-                let options = {
-                    headers: {
-                        "Authorization": "Bearer " + this.$store.state.credentials.accessToken
-                    }
-                }
-                this.$http.get(this.$store.state.apiURL + 'info/', options).then(response => {
-                    console.log(response)
-                }).catch(e => {
-                    console.log(e)
-                })
-            },
-			// Refreshes the access token
-			refreshToken() {
-            	let options = {
-            		headers: {
-            			'Content-Type': 'application/json'
-					}
-				}
-				this.$http.post(this.$store.state.apiURL + 'refresh/', {refresh: this.$store.state.credentials.refreshToken}, options).then(response => {
+				})
+		},
+		// Gets user info by providing a token
+		getInfo() {
+			let options = {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.credentials.accessToken,
+				},
+			}
+			this.$http
+				.get(this.$store.state.apiURL + 'info/', options)
+				.then(response => {
+					console.log(response)
+				})
+				.catch(e => {
+					console.log(e)
+				})
+		},
+		// Refreshes the access token
+		refreshToken() {
+			let options = {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+			this.$http
+				.post(
+					this.$store.state.apiURL + 'refresh/',
+					{ refresh: this.$store.state.credentials.refreshToken },
+					options
+				)
+				.then(response => {
 					let token = response.body.access
 					if (token != null) {
-						console.log("new access token fetched...")
+						console.log('new access token fetched...')
 						this.$store.state.credentials.accessToken = token
 						set('credentials', this.$store.state.credentials).then(() => {
 							console.log('token saved.')
 						})
-					}
-					else {
+					} else {
 						console.log("couldn't refresh token:")
 						console.log(response)
 					}
-				}).catch(e => {
-					console.log("error while refreshing token:")
+				})
+				.catch(e => {
+					console.log('error while refreshing token:')
 					console.log(e)
 				})
-			}
-        },
-    }
+		},
+	},
+}
 </script>
 
 <style>
-    @import './css/animate.min.css';
-    @import './css/style.css';
+@import './css/animate.min.css';
+@import './css/style.css';
 
-    #app {
-        overflow: hidden;
-    }
+#app {
+	overflow: hidden;
+}
 </style>
--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -1,100 +1,159 @@
 <!-- The conversation with a single contact shown in the middle -->
 <template>
-    <div>
+	<div>
+		<!-- Name of the contact at the top -->
+		<div class="user-wrapper row middle-xs">
+			<div class="col-xs" v-if="$store.state.selectedChat">
+				{{ $store.state.contacts[$store.state.selectedChat].name }}
+			</div>
+		</div>
 
-        <!-- Name of the contact at the top -->
-        <div class="user-wrapper row middle-xs">
-            <div class="col-xs" v-if="$store.state.selectedChat">
-                {{$store.state.contacts[$store.state.selectedChat].name}}
-            </div>
-        </div>
+		<!-- Messages of the conversation -->
+		<div class="messages-wrapper">
+			<messages
+				v-if="$store.state.selectedChat"
+				:chat="$store.state.chats[$store.state.selectedChat]"
+			/>
+		</div>
 
-        <!-- Messages of the conversation -->
-        <div class="messages-wrapper">
-            <messages v-if="$store.state.selectedChat" :chat="$store.state.chats[$store.state.selectedChat]"/>
-        </div>
-
-        <!-- Input field for sending messages -->
-        <div class="input-wrapper row middle-xs">
-            <div class="col-xs">
-                <input class="input-field" type="text" v-model="message" @keydown.enter="sendMessage">
-            </div>
-        </div>
-
-    </div>
+		<!-- Input field for sending messages -->
+		<div class="input-wrapper row middle-xs">
+			<div class="col-xs">
+				<input
+					class="input-field"
+					type="text"
+					v-model="message"
+					@keydown.enter="sendMessage"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import {get, set} from 'idb-keyval'
+import { get, set } from 'idb-keyval'
 
-    export default {
-        data() {
-            return {
-                message: "",
-            }
-        },
-        methods: {
-            // Sends a message
-            sendMessage() {
-                if (this.message.length == 0 || this.$store.state.selectedChat == null) return
-                // Post request for sending the message
-                let options = {
-                    headers: {
-                        'Authorization': 'Bearer ' + this.$store.state.credentials.accessToken
-                    }
-                }
-                // TODO encrypt message here using this.$store.state.contacts[this.$store.state.selectedChat].key
-                this.$http.post(
-                    this.$store.state.apiURL + 'send/',
-                    {
-                        receiver: this.$store.state.selectedChat,
-                        message: this.message
-                    },
-                    options
-                ).then((response) => {
-                    console.log("message sent.")
-                    console.log(response.body)
-                }, error => {
-                    console.log(error)
-                })
+export default {
+	data() {
+		return {
+			aesjs: null,
+			message: '',
+		}
+	},
+	created() {
+		this.aesjs = require('aes-js')
+	},
+	methods: {
+		// Sends a message
+		sendMessage() {
+			if (this.message.length == 0 || this.$store.state.selectedChat == null)
+				return
+			// Post request for sending the message
+			let options = {
+				headers: {
+					Authorization: 'Bearer ' + this.$store.state.credentials.accessToken,
+				},
+			}
+			let encryptedMessage = this.encryptMsg(this.message)
+			console.log(encryptedMessage)
 
-                // Stores the message locally
-                this.$store.state.chats[this.$store.state.selectedChat].push({received: false, content: this.message})
-                set('chats', this.$store.state.chats).then(() => {
-                    // scroll down to see the new message
-                    let mesDiv = document.getElementById("messages")
-                    mesDiv.scrollTop = mesDiv.scrollHeight
-                })
+			this.$http
+				.post(
+					this.$store.state.apiURL + 'send/',
+					{
+						receiver: this.$store.state.selectedChat,
+						message: encryptedMessage,
+					},
+					options
+				)
+				.then(
+					response => {
+						console.log('message sent.')
+						console.log(response.body)
+					},
+					error => {
+						console.log(error)
+					}
+				)
 
-                // clear input
-                this.message = ""
-            }
-        }
-    }
+			// Stores the message locally
+			this.$store.state.chats[this.$store.state.selectedChat].push({
+				received: false,
+				content: this.message,
+			})
+			set('chats', this.$store.state.chats).then(() => {
+				// scroll down to see the new message
+				let mesDiv = document.getElementById('messages')
+				mesDiv.scrollTop = mesDiv.scrollHeight
+			})
+
+			// clear input
+			this.message = ''
+		},
+		encryptMsg(message) {
+			// TODO encrypt message here using this.$store.state.contacts[this.$store.state.selectedChat].key
+			console.log(`original message: ${message}`)
+			console.log(
+				this.$store.state.contacts[this.$store.state.selectedChat].key
+			)
+
+			// TODO Generate a 256bit key from the shared key
+			//* An example 128-bit key
+			let key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+			// Convert the message to bytes
+			let textBytes = this.aesjs.utils.utf8.toBytes(message)
+
+			//* Setup our encryption mode
+			let aesCtr = new this.aesjs.ModeOfOperation.ctr(
+				key,
+				new this.aesjs.Counter(5)
+			)
+			//* Encrypt and convert to hex
+			let encryptedBytes = aesCtr.encrypt(textBytes)
+			let encryptedHex = this.aesjs.utils.hex.fromBytes(encryptedBytes)
+			console.log(`encrypted: ${encryptedHex}`)
+
+			// // Convert to bytes, then decrypt and show the original message
+			// aesCtr = new this.aesjs.ModeOfOperation.ctr(
+			// 	key,
+			// 	new this.aesjs.Counter(5)
+			// )
+
+			// let decryptedBytes = aesCtr.decrypt(encryptedBytes)
+			// let decryptedText = this.aesjs.utils.utf8.fromBytes(decryptedBytes)
+
+			// console.log(`Decrypted: ${decryptedText}`)
+
+			// TODO end of encryption
+			return encryptedHex
+		},
+	},
+}
 </script>
 
 <style scoped>
-    .user-wrapper {
-        height: 50px;
-        border-bottom: gray 1px solid;
-        font-weight: bold;
-        font-size: 20px;
-        padding: 10px;
-    }
-    .messages-wrapper {
-        height: calc(100vh - 100px);
-        overflow: hidden;
-    }
-    .input-wrapper {
-        border-top: gray 1px solid;
-        height: 50px;
-    }
-    .input-field {
-        width: calc(100% - 30px);
-        border-radius: 10px;
-        padding: 10px;
-        background-color: lightgray;
-        border: 0;
-        margin: 5px;
-    }
+.user-wrapper {
+	height: 50px;
+	border-bottom: gray 1px solid;
+	font-weight: bold;
+	font-size: 20px;
+	padding: 10px;
+}
+.messages-wrapper {
+	height: calc(100vh - 100px);
+	overflow: hidden;
+}
+.input-wrapper {
+	border-top: gray 1px solid;
+	height: 50px;
+}
+.input-field {
+	width: calc(100% - 30px);
+	border-radius: 10px;
+	padding: 10px;
+	background-color: lightgray;
+	border: 0;
+	margin: 5px;
+}
 </style>
